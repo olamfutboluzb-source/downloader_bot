f162067508bd8465f832ddc29e855a7d862c6ac7
olamfutboluzb-source <olamfutboluzb@gmail.com>
2026-01-21 23:14:54 +0500

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,20 +1,29 @@
-import yt_dlp
-import os  # untouchable download code , by me
-
-
 def download_video(url):
     outdir = 'downloads'
     os.makedirs(outdir, exist_ok=True)
 
     ydl_opts = {
-        # picking the quality of video into mp4 t, to download my n word
+        # ТВОИ НАСТРОЙКИ (не трогал)
         'format': 'bestvideo[height<=1080][vcodec^=avc1]+bestaudio[ext=m4a]/best[ext=mp4]/best',
         'outtmpl': f'{outdir}/%(title)s.%(ext)s',
         'merge_output_format': 'mp4',
         'noplaylist': True,
         'quiet': True,
-        # faststart is you can faster see the video when you are not fully finished downloading
         'postprocessor_args': ['-c', 'copy', '-movflags', '+faststart'],
+
+        # ДОБАВИЛ ЗАЩИТУ ДЛЯ YOUTUBE (чтобы качало)
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Referer': 'https://www.google.com/',
+        },
+        'extractor_args': {
+            'youtube': {
+                'player_client': ['android', 'web'],
+                'skip': ['dash', 'hls']
+            }
+        },
     }
 
     try:
